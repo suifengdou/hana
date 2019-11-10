5fb36df6fcadb19560d6e77412b174444f370887
sfd-macpro <767083786@qq.com>
2019-11-10 13:04:07 +0800

    V0.5.1.3

---
+++ apps/users/adminx.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# @Time : 2019/11/10 10:43 AM
+# @Author: Hann
+# @File: xadmin.py
+
+import xadmin
+
+from xadmin import views
+
+
+class GlobalSettings(object):
+    site_title = 'UT后台管理系统'
+    site_footer = 'UltraTool V0.5.1.0'
+    menu_style = 'accordion'
+
+xadmin.site.register(views.CommAdminView, GlobalSettings)
+
+
