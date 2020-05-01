3677fbb17013d4cb5cf2d84e882ffbcc18f1e45e
sfd-macpro <767083786@qq.com>
2020-05-01 15:46:10 +0800

    v0.5.1.31.1

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -10,7 +10,7 @@ from xadmin import views
 
 class GlobalSettings(object):
     site_title = 'UT后台管理系统'
-    site_footer = 'UltraTool V0.5.1.30'
+    site_footer = 'UltraTool V0.5.1.31.1'
     menu_style = 'accordion'
 
 
